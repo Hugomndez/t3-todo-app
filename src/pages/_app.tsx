f0f081e9f608c77fcba98a69aa7dbaf6834e6209
Hugomndez <hello@hugomendez.dev>
2023-01-24 12:46:03 -0500

    feat: add @next/font, josefin sans

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,33 @@
-import { type AppType } from 'next/app';
+import { Josefin_Sans } from '@next/font/google';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+import { type AppType } from 'next/app';
 
 import { api } from '../utils/api';
 
 import '../styles/globals.css';
+
+const josefinSans = Josefin_Sans({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${josefinSans.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
