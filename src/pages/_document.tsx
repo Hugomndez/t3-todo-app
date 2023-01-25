7170a4ac263e55fb4bb4dd23e83c5108203ae468
Hugomndez <hello@hugomendez.dev>
2023-01-24 20:05:33 -0500

    feat: add darkmode logic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,23 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+const setInitialTheme = `
+function getUserPreference() {
+  if(window.localStorage.getItem('usehooks-ts-dark-mode')) {
+    return window.localStorage.getItem('usehooks-ts-dark-mode') ? 'dark' : 'light'
+  }
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+}
+document.body.dataset.theme = getUserPreference();
+`;
+
 export default function Document() {
   return (
     <Html dir="ltr">
       <Head />
       <body>
+        <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
         <Main />
         <NextScript />
       </body>
