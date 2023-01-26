688ee489bf87dc367383062408c5244b6e2959e0
Hugomndez <hello@hugomendez.dev>
2023-01-25 19:02:49 -0500

    feat: styled darkmode btn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,6 +27,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
       `}</style>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="color-scheme" content="light dark" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SessionProvider session={session}>
